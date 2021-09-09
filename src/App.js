269dbee8b59a41c81fa219bd86f8d47682b69419
PJMantoss <joelptoss@gmail.com>
2021-09-09 17:34:40 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -40,14 +40,14 @@ function App() {
 
       <Spacer></Spacer>
 
-      <Center>
-      <Box mt="20" width="50%" p={5} bg="#161522" borderWidth="1px" borderRadius="md" overflow="hidden">
+      <Center width="100%">
+      <Box mt={20} width="50%" p={5} bg="#161522" borderWidth="1px" borderRadius="md" overflow="hidden">
         <Button colorScheme="teal" size="md">Swap</Button>
         
-        <Box mt="5" width="80%" p={3} bg="#202231" border="none" borderRadius="md">
+        <Box mt={5} py={8} px={5} bg="#202231" border="none" borderRadius="md">
           <InputGroup size="md">
             <InputLeftAddon children="simETH" />
-            <Input type="number" bg="#161522" border="none" placeholder="0.00" />
+            <Input ml={10} type="number" bg="#161522" border="none" placeholder="0.00" />
           </InputGroup>
         </Box>
       </Box>
