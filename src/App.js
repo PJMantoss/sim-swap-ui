ce1b73c3813f494da083eb944abb32f0888954ef
PJMantoss <joelptoss@gmail.com>
2021-09-09 14:28:38 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Web3Context } from "./web3";
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { Image } from "@chakra-ui/react";
+//https://www.dropbox.com/sh/3ykmghjcaordk7n/AAABIrQ6uulce6k7KENitQXRa?dl=0&preview=keko_logo_color.png
 
 function App() {
   const { account, connectWeb3, logout } = useContext(Web3Context);
@@ -15,7 +18,7 @@ function App() {
   return (
     <VStack p={3}>
       <Flex w="100%">
-        <Heading ml="8" size="md" fontWeight='semibold' color="cyan.400">swapUI</Heading>
+        <Heading ml="5" size="md" fontWeight='semibold' color="cyan.400">swapUI</Heading>
       </Flex>
     </VStack>
   );
