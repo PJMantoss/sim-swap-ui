53cd3a9e6542015cc59e0b9bf662465d187edc26
PJMantoss <joelptoss@gmail.com>
2021-09-09 14:37:34 +0100

    update App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,6 +10,7 @@ import { Web3Provider } from "./web3";
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
+    <ColorModeScript initialColorMode="dark"></ColorModeScript>
       <Web3Provider>
         <App />
       </Web3Provider>
