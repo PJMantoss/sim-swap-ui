d12119cbf715d4b0163dab83a767f71dbf1209ba
PJMantoss <joelptoss@gmail.com>
2021-09-11 16:49:07 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -140,9 +140,6 @@ function App() {
               </h2>
               <AccordionPanel pb={4}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
               </AccordionPanel>
             </AccordionItem>
 
@@ -157,9 +154,6 @@ function App() {
               </h2>
               <AccordionPanel pb={4}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
               </AccordionPanel>
             </AccordionItem>
           </Accordion>
