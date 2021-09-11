dd8b07fbfcf73824b6cd3f3e29449620f7f287e7
PJMantoss <joelptoss@gmail.com>
2021-09-11 10:25:07 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -34,8 +34,6 @@ function App() {
       >
         <Heading ml="5" size="md" fontWeight='semibold' color="cyan.400">swapUI</Heading>
 
-        <Spacer></Spacer>
-
         <Button
           colorScheme="teal"
           size="sm"
@@ -54,7 +52,9 @@ function App() {
           Rinkeby <ExternalLinkIcon mx="2px" />
         </Link>
 
-        <Button colorScheme="teal" size="md">Connect to a wallet</Button>
+        <Spacer></Spacer>
+
+        <Button colorScheme="teal" size="sm">Connect to a wallet</Button>
         
         <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
       </Flex>
