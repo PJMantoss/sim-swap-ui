67cb9251e72aaf846cfdda00e2fe62556b51f756
PJMantoss <joelptoss@gmail.com>
2021-09-11 09:18:37 +0100

    add new components

---
+++ src/components/Prices.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Prices = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Prices.propTypes = {
+    
+};
+
+export default Prices;
