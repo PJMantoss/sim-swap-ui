98c7c0390449a2fe95b1b5c41afa7460ff4ada8a
PJMantoss <joelptoss@gmail.com>
2021-09-12 15:24:48 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -5,6 +5,18 @@ const coinGeckoClient = new CoinGecko();
 
 
 const Prices = props => {
+    const { data } = props.result;
+  //console.log(data);
+
+  const formatPercent = num => `${new Number(num).toFixed(2)}%`;
+
+  const formatDollar = (number, maximumSignificantDigits) => 
+      new Intl.NumberFormat('en-Us', {
+        style: 'currency',
+        currency: 'usd',
+        maximumSignificantDigits
+      }).format(number);
+      
     return (
         <div>
             
