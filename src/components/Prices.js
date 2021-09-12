a38e0e9ffbd80d1f9f1496647aff419d3dcbb9f2
PJMantoss <joelptoss@gmail.com>
2021-09-12 20:37:21 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,35 +1,7 @@
 import React from 'react';
-import CoinGecko from 'coingecko-api'
-
-const coinGeckoClient = new CoinGecko();
 
 
 const Prices = props => {
-
-    async function getCoinData(){
-        const params = {
-          order: CoinGecko.ORDER.MARKET_CAP_DESC
-        };
-        
-        const result = await coinGeckoClient.coins.markets({params});
-        return {
-          props: {
-            result
-          }
-        };
-      }
-
-    const { data } = props.result;
-    console.log(data);
-
-    const formatPercent = num => `${new Number(num).toFixed(2)}%`;
-
-    const formatDollar = (number, maximumSignificantDigits) => 
-        new Intl.NumberFormat('en-Us', {
-            style: 'currency',
-            currency: 'usd',
-            maximumSignificantDigits
-        }).format(number);
 
     return (
         <div>
