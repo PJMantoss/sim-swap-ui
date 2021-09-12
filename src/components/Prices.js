7053dfcb964c98bc3a140bcdd970e20528174178
PJMantoss <joelptoss@gmail.com>
2021-09-12 20:42:38 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+  } from "@chakra-ui/react"
 
 
 const Prices = props => {
