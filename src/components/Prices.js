7e2e6ad35ab88af86c4e6ed3d51a2da5d0a604b3
PJMantoss <joelptoss@gmail.com>
2021-09-12 21:08:33 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -15,6 +15,10 @@ const Prices = props => {
 
     const getCoins = async () => {
         const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+        try{}catch(err){
+            console.error(err);
+        }
     }
 
     return (
