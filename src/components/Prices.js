3aa5dddac08b59ce76cef414ede4ee667f99e3b5
PJMantoss <joelptoss@gmail.com>
2021-09-13 09:57:51 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Table,
     Thead,
@@ -12,6 +12,7 @@ import {
 
 
 const Prices = async (props) => {
+    const [coins, setCoins] = useState([]);
 
         const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
