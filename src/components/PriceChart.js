5de248d6f3c505bbc3bffce1212b0dd579a28253
PJMantoss <joelptoss@gmail.com>
2021-09-13 15:34:24 +0100

    add media-query

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Graph from './Graph';
 
 const PriceChart = props => {
+    const [currencies, setCurrencies] = useState([]);
+    const [pair, setPair] = useState("");
+    const [price, setPrice] = useState("0.00");
+    const [pastData, setPastData] = useState({});
+    const ws = useRef(null);
+
     return (
         <div>
             
