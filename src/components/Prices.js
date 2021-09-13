b7adf4b732a20994a88747b4cd966c75d6fbf13c
PJMantoss <joelptoss@gmail.com>
2021-09-13 13:31:29 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -11,20 +11,13 @@ import {
   } from "@chakra-ui/react"
 
 
-const Prices = async (props) => {
-    const [coins, setCoins] = useState([]);
+const Prices = async () => {
+    
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
-    const getCoins = async () => {
-        const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
-        try{
-            const coinsPromise = await fetch(url);
-            const coins = await coinsPromise.json();
-            console.log(coins);
-        }catch(err){
-            console.error(err);
-        }
-    }
+    const coinsPromise = await fetch(url);
+    const data = await coinsPromise.json();
+    console.log(data);
 
     return (
         <Table variant="striped" colorScheme="teal">
@@ -38,8 +31,8 @@ const Prices = async (props) => {
                 </Tr>
             </Thead>
             <Tbody>
-                {coins.map((coin) => (
-                    <Tr key={coin.id}>
+                {data.map((coin, idx) => (
+                    <Tr key={idx}>
                         <Td>{coin.symbol.toUpperCase()}</Td>
                         <Td></Td>
                         <Td></Td>
