ea480058497de78a828ede395eb8bad745439456
PJMantoss <joelptoss@gmail.com>
2021-09-17 19:20:23 +0100

    update Graph

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Graph from './Graph';
 import { formatData } from '../formatter';
-import { Select } from "@chakra-ui/react"
+import { Select, Flex, Box } from "@chakra-ui/react"
 
 const PriceChart = props => {
     const [currencies, setCurrencies] = useState([]);
