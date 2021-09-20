60d57dc893d5d2915f0c64f7aa0dc84dd91c3397
PJMantoss <joelptoss@gmail.com>
2021-09-20 19:46:37 +0100

    update priceChart

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ function App() {
 
           <Accordion 
               mt={isNotSmallerScreen ? "5" : "3"} 
-              width={isNotSmallerScreen ? "75%" : "98%"}
+              width={isNotSmallerScreen ? "75%" : "100%"}
           >
             <AccordionItem>
               <h2>
